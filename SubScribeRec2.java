c8210bf1922426f9a6a778f7bc020fcff1fbb9be
ZhangYunfei <15711071337@163.com>
2019-06-05 10:59:05 +0800

    RabbitMQ之订阅模式的消费者2
    RabbitMQ之订阅模式的消费者2

---
+++ SubScribeRec2.java
@@ -0,0 +1,55 @@
+package com.zyf.subscribe.fanout;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.zyf.help.ConnectHelp;
+
+/**
+ * 订阅 消费者1
+ * 
+ * @author zhang
+ *
+ */
+public class SubScribeRec2 {
+	public static final String EXCHANGE_NAME = "test_exchange";
+	public static final String QUEUE_NAME = "test_queue_2";
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+
+		// 连接
+		Connection connect = ConnectHelp.getConnect();
+
+		// 获取通道
+		Channel channel = connect.createChannel();
+
+		// 创建队列
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		// 绑定队列
+		channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "");
+
+		// 只接受一个
+		channel.basicQos(1);
+		// 接收数据
+		DefaultConsumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+					throws IOException {
+
+				String msg = new String(body);
+				System.out.println("订阅2：" + msg);
+
+			}
+		};
+
+		// 监听数据
+		channel.basicConsume(QUEUE_NAME, false, consumer);
+
+	}
+
+}
